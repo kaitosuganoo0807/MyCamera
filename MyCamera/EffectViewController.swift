ae1932e70eba2195d97450f65811e1da846742fc
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-15 16:40:06 +0900

    エフェクト機能追加

--- MyCamera/EffectViewController.swift
+++ MyCamera/EffectViewController.swift
@@ -25,12 +25,47 @@ class EffectViewController: UIViewController {
     
     @IBOutlet weak var effectImage: UIImageView!
     
+    // フィルタ名を列挙した配列(Array)
+    
+    // 0.モノクロ
+    // 1.Chrome
+    // 2.Fade
+    // 3.Instant
+    // 4.Noir
+    // 5.Process
+    // 6.Tonal
+    // 7.Transfer
+    // 8.Sepia Tone
+    let filterArray = ["CIPhotoEffectMono",
+                        "CIPhotoEffectChrome",
+                        "CIPhotoEffectFade",
+                        "CIPhotoEffectInstant",
+                        "CIPhotoEffectNoir",
+                        "CIPhotoEffectProcess",
+                        "CIPhotoEffectTonal",
+                        "CIPhotoEffectTransfer",
+                        "CISepiaTone"
+    ]
+    
+    // 選択中のエフェクト添字
+    var filterSelectNumber = 0
+    
     @IBAction func effectButtonAction(_ sender: Any) {
         // エフェクト前画像をアンラップしてエフェクト用画像として取り出す
         if let image = originalImage {
             
             // フィルター名を設定
-            let filterName = "CIPhotoEffectMono"
+            let filterName = filterArray[filterSelectNumber]
+            
+            // 次の選択するエフェクト添字に変更
+            filterSelectNumber += 1
+            
+            // 添字が配列の数と同じか？チェック
+            if filterSelectNumber == filterArray.count {
+                
+                // 同じ場合は最後まで選択されたので先順に戻す
+                filterSelectNumber = 0
+            }
             // 元々の画像の回転角度を取得
             let rotate = image.imageOrientation
             // UIImage形式の画像をCIImage形式に変換
@@ -50,12 +85,33 @@ class EffectViewController: UIViewController {
                 return
             }
             // CIContextのインスタンスを取得
+            let ciContext = CIContext(options: nil)
             
+            // エフェクト後の画像をCIContext上に描画し、結果をcgImage形式の画像を取得
+            guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else {
+                return
+            }
+            // エフェクト後の画像をCGImage形式からUIImage形式に変換。その際に回転角度を指定。そしてImageViewに表示
+            effectImage.image = UIImage(cgImage: cgImage, scale: 1.0, orientation: rotate)
         }
         
     }
     
     @IBAction func shareButtonAction(_ sender: Any) {
+        
+        if let shareImage = effectImage.image {
+            
+            let shareItems = [shareImage]
+            
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            
+            // シェアボタンの情報を取得
+            let button = sender as! UIButton
+            
+            controller.popoverPresentationController?.sourceView = button
+            
+            present(controller, animated: true, completion: nil)
+        }
     }
     
     @IBAction func closeButtonAction(_ sender: Any) {
